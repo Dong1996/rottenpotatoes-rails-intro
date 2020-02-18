b7e905582e1626186c2176c401d2af3357db12fd
EC2 Default User <ec2-user@ip-172-31-39-40.us-east-2.compute.internal>
2020-02-18 05:49:06 +0000

    Part 1 Done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,43 +11,19 @@ class MoviesController < ApplicationController
   end
 
   def index
-     @all_ratings = Movie.rating
-     if(params[:ratings] != NIL)
-        session[:ratings] = params[:ratings]
-     end
-     if(params[:sort] != NIL)
-        session[:sort] = params[:sort]
-     end
-     if(params[:id] != NIL)
-        session[:id] = params[:id]
-     end
-     
-     if(((params[:ratings] == NIL) && !(session[:ratings] == NIL) ) || (params[:sort] == NIL)  && !(session[:sort] == NIL))
-        redirect_to movies_path("ratings" => session[:ratings], "sort" => session[:sort])
-     else 
-       if(params[:ratings] != NIL && params[:sort] != NIL)
-         rating_array = params[:ratings].keys
-         @movies = Movie.where(rating: rating_array).order(params[:sort])
-       elsif(params[:sort] != NIL)
-         @movies = Movie.order(params[:sort])
-       else
-         @movies = Movie.all
-       end
-     end
-
-     instance_variable_set("@#{session[:id]}_hilite", "hilite")
+    @movies = Movie.order(params[:sort])
   end
-  
+
   def new
     # default: render 'new' template
   end
-  
+
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
   end
-  
+
   def edit
     @movie = Movie.find params[:id]
   end
@@ -65,6 +41,5 @@ class MoviesController < ApplicationController
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
   end
-  
 
 end
